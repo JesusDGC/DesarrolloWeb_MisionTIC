249d09d0573dd57d404ec627b15293171eac64b1
jeimiromero <jeimi.romero@gmail.com>
2021-10-04 15:52:08 -0500

    clase Registrar Ejemplar

---
+++ Ganaderia.App/Ganaderia.App.Dominio/Entidades/RegistrarEjemplar.cs
@@ -0,0 +1,11 @@
+namespace Ganaderia.App.Dominio
+{
+    public class RegistrarEjemplar
+
+    {
+        public int IdLote { get; set; }
+        public int IdEjemplar { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string Raza{ get; set; }
+     }
+}
